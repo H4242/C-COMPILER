aa06b52fea8c804947d4e976663b3f88d76f6df0
Yahia Bellamine <92577857+YahiaBellamine@users.noreply.github.com>
2023-03-10 09:10:05 +0100

    Revert "runmake_dept.sh added"
    This reverts commit 325e654cef0bd8678e04b5323c43c6b52650438b.

--- compiler/runmake_dept.sh
+++
@@ -1,6 +0,0 @@
-# to get the required packages (ubuntu 21.10):
-#   sudo apt install     antlr4 libantlr4-runtime-dev default-jdk
-# the ANTLRJAR part below is copied from /usr/bin/antlr4
-make ANTLRJAR=../antlr/jar/antlr-4.9.2-complete.jar ANTLRINC=../antlr/include ANTLRLIB=../antlr/lib/libantlr4-runtime.a "$@"
-
-# You can override these from the command-line, as illustrated by the various `runmake_*.sh` scripts
