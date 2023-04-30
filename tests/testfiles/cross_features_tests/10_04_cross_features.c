d879a98c90d9ef6904ee1c0e5e8807a8a3f9313d
abou961 <jaboumouss@LAPTOP-VQ5DN98J>
2023-04-30 15:14:46 +0200

    🔥 Moved cross features testfiles

---
+++ tests/testfiles/cross_features_tests/10_04_cross_features.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void print_int(int x)
+{
+    if (x < 0)
+    {
+        putchar('-');
+        x = -x;
+    }
+    if (x / 10 != 0)
+    {
+        print_int(x / 10);
+    }
+
+    putchar(x % 10 + '0');
+}
+int main()
+{
+    print_int(-273);
+    putchar(10); // newline
+    return 0;
+}
